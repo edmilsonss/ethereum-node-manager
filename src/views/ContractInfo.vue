b3ca8995b4145616d44abcd2ce9df4ffdbc8e838
Edmilson Santana <edmilson.santana@gmail.com>
2018-11-20 11:48:36 -0300

    Project Creation

---
+++ src/views/ContractInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contract-info">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <SCBasicInfo msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SCBasicInfo from '@/components/SCBasicInfo.vue'
+
+export default {
+  name: 'contract-info',
+  components: {
+    SCBasicInfo
+  }
+}
+</script>
